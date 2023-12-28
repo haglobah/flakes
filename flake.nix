65a292df34a71a4fbe8efe36fc2c26224f376320
Beat Hagenlocher <bah@posteo.de>
2023-12-28 16:51:20 +0100

    Add node to templates

--- flake.nix
+++ flake.nix
@@ -13,6 +13,10 @@
         path = ./templates/racket;
         description = "A flake-parts flake with devshell and racket set up";
       };
+      node = {
+        path = ./templates/node;
+        description = "A flake-parts flake with devshell and nodejs set up";
+      };
       deploy = {
         path = ./templates/deploy;
         description = "A flake-parts flake with devshell, vm and deployment capabilities set up";

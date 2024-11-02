398f935ad8e3c242131ba52809075ff7dba80065
Beat Hagenlocher <bah@posteo.de>
2024-11-02 23:43:57 +0100

    Add to pre-commit-hooks to gitignore

--- templates/node/flake.nix
+++ templates/node/flake.nix
@@ -1,51 +1,57 @@
 {
-  description = "A project with a devshell.";
-
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*.tar.gz";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     devshell.url = "github:numtide/devshell";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         inputs.devshell.flakeModule
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-
+        inputs.pre-commit-hooks.flakeModule
       ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        pre-commit = {
+          settings.hooks = {
+            alejandra.enable = true;
+            deadnix.enable = true;
+          };
+        };
         # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
+        # packages.default = pkgs.hello;
         devshells.default = {
           env = [
             # { name = "MY_ENV_VAR"; value = "SOTRUE"; }
           ];
-          packages = [
-            pkgs.nodejs_22
-            pkgs.pnpm
-          ];
           commands = [
-            { name = "run"; command = "pnpm dev"; help = "A command to test devshell";}
+            {
+              name = "run";
+              command = "pnpm dev";
+              help = "(pnpm dev):Run the development environment";
+            }
           ];
-          devshell.startup = {
-            install-npm-dependencies.text = ''pnpm install'';
-          }
+          devshell = {
+            packagesFrom = [
+            ];
+            packages = [
+              pkgs.nodejs_22
+              pkgs.pnpm
+            ];
+            startup = {
+              install-npm-dependencies.text = ''pnpm install'';
+              pre-commit.text = config.pre-commit.settings.installationScript;
+            };
+          };
         };
       };
       flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
       };
     };
 }
